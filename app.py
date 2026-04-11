f53e36a4a4e3acfa9c72a514f7fb729972b95594
Christian <christian.nascimento.falcao@gmail.com>
2026-04-11 13:34:53 -0300

    Removendo lixo do projeto! Adicionando descricao as variaveis dinamicas! Adicionando .gitignore Definindo versao para requiriments.txt para nao haver problemas relacionado a divergencias de atualizacao de pacotes!

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ from database import db
 from controller.usuario_controller import usuario_bp
 from controller.dispositivo_controller import dispositivo_bp
 
+# Variaveis dinamicas que sao usadas para definir qual a porta do servidor e o host do mesmo
+host = str(os.environ.get("HOST", "localhost"))
+port = int(os.environ.get("PORT", 5000))
+debug = str(os.environ.get("DEBUG", "true"))
+
+# Variaveis dinamicas usadas para definir o Host do banco de dados assim conectando a banco de dados dedicado
+db_user = str(os.environ.get("POSTGRES_DB_KEY", "fauUser"))
+db_name = str(os.environ.get("POSTGRES_USER_KEY", "facu"))
+db_host = str(os.environ.get("POSTGRES_HOST_KEY", "127.0.0.1"))
+db_password = int(os.environ.get("POSTGRES_PASSWORD_KEY", 123456789))
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///graphics_analytics.db'
 
@@ -15,7 +26,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    host = str(os.environ.get("HOST", "localhost"))
-    port = int(os.environ.get("PORT", 5000))
-    debug = str(os.environ.get("DEBUG", "true"))
     app.run(debug=debug, port=port, host=host)
